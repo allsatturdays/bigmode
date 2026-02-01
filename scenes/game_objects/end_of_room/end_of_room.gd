d40905885de142d7f3ff07a67d69421a795f33c6
mig <miguelv.dev@gmail.com>
2026-02-01 13:03:29 -0500

    fixed bug where dialogue triggers even after player death

--- scenes/game_objects/end_of_room/end_of_room.gd
+++ scenes/game_objects/end_of_room/end_of_room.gd
@@ -1,5 +1,14 @@
 extends Node3D
 
+var is_active: bool = true
+
+func _ready() -> void:
+	GameEvents.on_player_death.connect(_on_player_death)
+
+
 func _on_static_body_3d_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and is_active:
 		GameEvents.on_room_complete.emit(Main.game_controller.building.next_room())
+
+func _on_player_death() -> void:
+	is_active = false
